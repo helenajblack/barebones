38f4f94812e0f0f42ae667db9f6ef1d49609b3b5
Helena Black <helenajblack@yahoo.com>
2017-06-14 11:12:21 -0500

    template complete

---
+++ README.md
@@ -0,0 +1,16 @@
+## Barebones Template
+
++ GOAL: Create a barebones template to use for projects. Template will allow for changes, but is created with the idea that express, body-parser, pg, angular, path, and ____ will be installed.
+
++ TECHNOLOGIES: angular js, ______
+
++ EXAMPLES AVAILABLE: routes,
+
++ TO DO :
+
+[]- mkdir for project
+[]- initialize github repo and link to project
+[]- install express, body-parser, pg, angular, path, and ______
+[]- update file path names (if needed)
+
+## PROJECT NAME:
---
+++ public/scripts/controller.js
@@ -0,0 +1,31 @@
+var app = angular.module('myApp', []);
+
+
+//route examples
+// app.config(function($routeProvider) {
+//   $routeProvider.when('/lisbeth', {
+//       templateUrl: 'views/routes/lisbethSalander.html',
+//       controller: 'lisbethSalander as ls'
+//     })
+//     .when('/buffy', {
+//       templateUrl: 'views/routes/buffySummers.html',
+//       controller: 'buffySummers as bs'
+//     })
+//     .when('/rue', {
+//       templateUrl: 'views/routes/rue.html',
+//       controller: 'rue as ru'
+//     });
+// });
+//
+// app.controller('lisbethSalander', lisbethSalander);
+// app.controller('buffySummers', buffySummers);
+// app.controller('rue', rue);
+//
+// function lisbethSalander() {
+//   console.log('Lisbeth Salander loaded');
+// }
+//
+// function buffySummers() {
+//   console.log('Buffy Summers loaded');
+// }
+//
